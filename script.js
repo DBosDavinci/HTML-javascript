bdacadd3e7342d45cfaabc332dd03f38d86670e3
DBosDavinci <99069008@mydavinci.nl>
2021-11-03 10:27:23 +0100

    working lightswitch

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ document.body.appendChild(btn);
 
 // schijf hier tussen je code
 
+btn.innerHTML = 'button is off';
+document.body.style.backgroundColor = "black";
+var flip = false;
 
+btn.onclick = function(){
+    if (flip == false){
+        btn.innerHTML = 'button is on';
+        document.body.style.backgroundColor = "yellow";
+        console.log("button is on");
+        flip = true;}
+    else {
+        btn.innerHTML = 'button is off';
+        document.body.style.backgroundColor = "black";
+        console.log("button is off");
+        flip = false;}
+};
 
 // schijf hier tussen je code
